83d020d3bf2ef1e3bd5be1ebe26e57c18eb22032
Christopher Rempe <43194904+MrRicearonie@users.noreply.github.com>
2022-07-19 13:33:46 -0700

    Moved from colormind api to huemint for https

--- assets/mixins/colors.js
+++ assets/mixins/colors.js
@@ -1,39 +1,46 @@
 import axios from "axios";
 
-var url = "http://colormind.io/api/";
+var url = "https://api.huemint.com/color";
 var data = {
-    model: "default",
-    input: ["N","N","N","N","N"]
+    "mode":"transformer", // transformer, diffusion or random
+    "num_colors":5, // max 12, min 2
+    "temperature":"1.2", // max 2.4, min 0
+    "num_results":1, // max 50 for transformer, 5 for diffusion
+    "adjacency":["0", "65", "45", "35", "15", "65", "0", "35", "65", "35", "45", "35", "0", "35", "45", "35", "65", "35", "0", "15", "15", "35", "45", "15", "0"], // nxn adjacency matrix as a flat array of strings
+    "palette":["-", "-", "-", "-", "-"], // locked colors as hex codes, or '-' if blank
+};
+var customConfig = {
+    headers: {
+        'Content-Type': 'application/json'
+    }
 };
 var results = [];
 
-function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-}
-
-function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-
+// Use the color provided to set the colors on the correct DOM elements
 function setColor(color, pos) {
     console.log(color);
     pos++;
     var backgrounds = document.getElementsByClassName("background_color"+pos);
     var text = document.getElementsByClassName("text_color"+pos);
     var borders = document.getElementsByClassName("border_color"+pos);
+
+    // Set the background color
     for (var i in backgrounds) {
         console.log(backgrounds[i]);
         if (backgrounds.hasOwnProperty(i)) {
             backgrounds[i].style.backgroundColor = color;
         }
     }
+
+    // Set the text color
     for (var i in text) {
         console.log(text[i]);
         if (text.hasOwnProperty(i)) {
             text[i].style.color = color;
         }
     }
+
+    // Set the border color
     for (var i in borders) {
         console.log(borders[i]);
         if (borders.hasOwnProperty(i)) {
@@ -44,28 +51,23 @@ function setColor(color, pos) {
 
 export const colorMixin = {
     methods: {
+        // Get colors from Huemint and the set the colors on the site
         getColors() {
-            axios({
-                method: 'post',
-                url: url,
-                data: JSON.stringify(data)
-            })
-            .then((response) => {
-                results = response.data.result;
+            axios.post(url, JSON.stringify(data), customConfig).then((response) => {
+                results = response.data.results[0].palette;
                 console.log(results);
                 for (var i in results) {
-                    setColor(rgbToHex(results[i][0], results[i][1], results[i][2]), i);
+                    setColor(results[i], i);
                 }
             }, (error) => {
                 console.log(error);
             });
         },
+        // When moving between pages, refresh is needed to set the colors up again
         refresh() {
-            console.log('test');
             for (var i in results) {
-                setColor(rgbToHex(results[i][0], results[i][1], results[i][2]), i);
+                setColor(results[i], i);
             }
-            console.log('testend')
         }
     }
 }
